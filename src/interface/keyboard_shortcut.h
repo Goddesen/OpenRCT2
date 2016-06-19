f025770e08897a0a7f5ca56443e40aa670fa4e22
Goddesen <hugowallenburg@gmail.com>
2016-06-19 04:06:15 +0200

    Mostly remove keyboard handling through scancodes

--- src/interface/keyboard_shortcut.h
+++ src/interface/keyboard_shortcut.h
@@ -17,11 +17,11 @@
 #ifndef _INTERFACE_KEYBOARD_SHORTCUT_H_
 #define _INTERFACE_KEYBOARD_SHORTCUT_H_
 
-#define SHORTCUT_UNDEFINED 0xFFFF
+#define SHORTCUT_UNDEFINED { SDLK_UNKNOWN, KMOD_NONE }
 
-void keyboard_shortcut_set(int key);
-void keyboard_shortcut_handle(int key);
+void keyboard_shortcut_set(keypress key);
+void keyboard_shortcut_handle(keypress key);
 void keyboard_shortcut_handle_command(int shortcutIndex);
-void keyboard_shortcut_format_string(char *buffer, uint16 shortcutKey);
+void keyboard_shortcut_format_string(char *buffer, keypress shortcutKey);
 
 #endif
